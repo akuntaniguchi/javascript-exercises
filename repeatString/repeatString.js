94591be295829908193799c38a6f68caf19e25ba
Akun Taniguchi <akunbtaniguchi@gmail.com>
2021-03-28 19:01:17 -0500

    Complete repeat string exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeat_number) {
+    result = ""
 
+    if(repeat_number < 0) {
+        return "ERROR"
+    }
+    
+    for(let i = 0; i < repeat_number; i++) {
+        result += string
+    }
+
+    return result
 }
 
 module.exports = repeatString
